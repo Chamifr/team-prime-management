99234b344d80f34aa3e22a2cf6eb5eadd8c4c7ba
Faqrulrazi bin Muhammad Yunus <faqrullogan@gmail.com>
2025-05-31 12:00:03 +0800

    reset project

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,12 +2,6 @@ package org.example;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("****************************************");
-        System.out.println("Welcome to Team Prime Management System");
-        System.out.println("****************************************");
-
-        System.out.println("1.Sign Up");
-        System.out.println("2.Log In");
-        System.out.println("3.Exit");
+        System.out.println("Welcome To Team Prime Management System");
     }
 }
